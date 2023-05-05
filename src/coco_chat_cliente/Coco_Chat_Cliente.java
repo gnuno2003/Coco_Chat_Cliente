62c654c7c1bbb6fae8b2b1c761885f9006183418
gnuno2003 <gnuno2003@gmail.com>
2023-05-05 11:35:07 -0600

    Se inicio el proyecto del cliente

---
+++ src/coco_chat_cliente/Coco_Chat_Cliente.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package coco_chat_cliente;
+
+/**
+ *
+ * @author gnuno
+ */
+public class Coco_Chat_Cliente {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
