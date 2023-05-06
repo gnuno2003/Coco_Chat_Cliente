ac72f7dc81c40a62b07898245b9d1b00df1c3b5f
gnuno2003 <gnuno2003@gmail.com>
2023-05-05 18:50:27 -0600

    Se actualizo la info enviada por cliente

--- src/coco_chat_cliente/Coco_Chat_Cliente.java
+++ src/coco_chat_cliente/Coco_Chat_Cliente.java
@@ -1,18 +1,30 @@
 package coco_chat_cliente;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 
 public class Coco_Chat_Cliente {
 
 
-    public static void main(String[] args) throws IOException {
+public static void main(String[] args) throws IOException {
         // TODO code application logic here
-        Socket s = new Socket("192.168.84.44", 1234);
-        s.getOutputStream().write("Hola UwU".getBytes());
-        //System.out.println(s.getInputStream().read());
-        System.out.println(s.getInputStream().read());
+        Socket s = new Socket("10.147.17.231", 1234);
+
+        PrintWriter writer = new PrintWriter(new OutputStreamWriter(s.getOutputStream(), "UTF-8"), true);
+        String mensaje = "¡Hola, mundo! ¿Cómo estás?";
+        writer.println(mensaje);
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(s.getInputStream(), "UTF-8"));
+
+        String mensajeRecibido = reader.readLine();
+        System.out.println("El mensaje del servidor es: " + mensajeRecibido);
+        writer.close();
+        s.close();
     }
 
     
